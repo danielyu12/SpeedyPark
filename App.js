d77ca9dbd68470207827d537d268672f146c6854
Justin Lee <justin.chaemin.lee@gmail.com>
2022-10-16 21:31:41 -0400

    in progress list of data

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, {useState} from "react";
+import {Dropdown}  from 'react-native-material-dropdown';
+import spots from '../scripts/CreateStreetObject.js';
+
 
 export default function App() {
+  const[selectedItem, setselectedItem] = useState(null)
+  const onSelect = (item) => {
+    setselectedItem(item)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Dropdown
+        value = {selectedItem}
+        data = {spots}
+        onSelect = {onSelect}
+      /> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 100,
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Src / Components/Dropdown.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+
+
+
+const Streets = ({
+    data,
+}) => {
+    console.log("selected value", value)
+    const [showOption, setshowOption] = useState(false) 
+    return (
+        <View>
+
+
+        </View>
+    )
+}

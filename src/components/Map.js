8165158611671bf17dcea0e1a2e749e8724cdeee
alex-zhou13 <75346304+alex-zhou13@users.noreply.github.com>
2022-11-06 17:43:03 -0500

    turnover data added, but hardcoded

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,9 +5,11 @@ import spots from '../../assets/StreetParking.json';
 import spotnumbers from '../../scripts/CreateStreetSpotNumberObject.js';
 import transactions from '../../assets/ParkBostonV1.json';
 import transactionsList from '../../scripts/CreateTransactionZoneObject.js';
+import turnovers from '../../assets/ParkBostonFiltered.json';
 
 var number2 = {};
 var displaynumber2 = {};
+var displayTurnover = {};
 
 function parseTransaction() {
   transactions.map((transaction, index) => {
@@ -17,8 +19,21 @@ function parseTransaction() {
   });
 }
 
+function parseTurnovers() {
+  turnovers.map((zone, index) => {
+    displayTurnover[0] = 'Sunday turnover: ' + zone.data.Sunday.averageTurnoverDay +
+                            '\n Monday turnover: ' + zone.data.Monday.averageTurnoverDay + 
+                            '\n Tuesday turnover: ' + zone.data.Tuesday.averageTurnoverDay + 
+                            '\n Wednesday turnover: ' + zone.data.Wednesday.averageTurnoverDay +
+                            '\n Thursday turnover: ' + zone.data.Thursday.averageTurnoverDay +
+                            '\n Friday turnover: ' + zone.data.Friday.averageTurnoverDay +
+                            '\n Saturday turnover: ' + zone.data.Saturday.averageTurnoverDay;
+  })
+}
+
 export default function Map() {
   parseTransaction();
+  // parseTurnovers();
 
   return (
     <View style={styles.container}>
@@ -35,16 +50,17 @@ export default function Map() {
         {spots.map((spot, index) => {
           const number =
             spotnumbers[spot.STREET][spot.BLK_NO]['quantity'].toString();
-          const displaynumber =
-            number +
-            ' potential spots on ' +
-            spot.STREET +
-            ' near ' +
-            spot.BLK_NO;
+          const displaynumber = number + ' spots on ' + spot.STREET + ' near ' + spot.BLK_NO;
           if (spot.X == -71.1134666081092 && spot.Y == 42.350702669858) {
             return (
               <Marker
-                description={displaynumber + ' with ' + displaynumber2[833]}
+                description={displaynumber + ' with ' + 'Sunday turnover: ' + String(turnovers[833].data.Sunday.averageTurnoverDay).slice(0,5) +
+                ', Monday turnover: ' + String(turnovers[833].data.Monday.averageTurnoverDay).slice(0,5) + 
+                ', Tuesday turnover: ' + String(turnovers[833].data.Tuesday.averageTurnoverDay).slice(0,5) + 
+                ', Wednesday turnover: ' + String(turnovers[833].data.Wednesday.averageTurnoverDay).slice(0,5) +
+                ', Thursday turnover: ' + String(turnovers[833].data.Thursday.averageTurnoverDay).slice(0,5) +
+                ', Friday turnover: ' + String(turnovers[833].data.Friday.averageTurnoverDay).slice(0,5) +
+                ', Saturday turnover: ' + String(turnovers[833].data.Saturday.averageTurnoverDay).slice(0,5)}
                 key={index}
                 coordinate={{
                   latitude: spot.LATITUDE,
@@ -56,7 +72,13 @@ export default function Map() {
           } else if (spot.X == -71.1132356080516 && spot.Y == 42.351016669925) {
             return (
               <Marker
-                description={displaynumber + ' with ' + displaynumber2[837]}
+                description={displaynumber + ' with ' + 'Sunday turnover: ' + String(turnovers[837].data.Sunday.averageTurnoverDay).slice(0,5) +
+                ', Monday turnover: ' + String(turnovers[837].data.Monday.averageTurnoverDay).slice(0,5) + 
+                ', Tuesday turnover: ' + String(turnovers[837].data.Tuesday.averageTurnoverDay).slice(0,5) + 
+                ', Wednesday turnover: ' + String(turnovers[837].data.Wednesday.averageTurnoverDay).slice(0,5) +
+                ', Thursday turnover: ' + String(turnovers[837].data.Thursday.averageTurnoverDay).slice(0,5) +
+                ', Friday turnover: ' + String(turnovers[837].data.Friday.averageTurnoverDay).slice(0,5) +
+                ', Saturday turnover: ' + String(turnovers[837].data.Saturday.averageTurnoverDay).slice(0,5)}
                 key={index}
                 coordinate={{
                   latitude: spot.LATITUDE,
@@ -68,7 +90,13 @@ export default function Map() {
           } else if (spot.X == -71.116764609232 && spot.Y == 42.3513896698829) {
             return (
               <Marker
-                description={displaynumber + ' with ' + displaynumber2[839]}
+                description={displaynumber + ' with ' + 'Sunday turnover: ' + String(turnovers[839].data.Sunday.averageTurnoverDay).slice(0,5) +
+                ', Monday turnover: ' + String(turnovers[839].data.Monday.averageTurnoverDay).slice(0,5) + 
+                ', Tuesday turnover: ' + String(turnovers[839].data.Tuesday.averageTurnoverDay).slice(0,5) + 
+                ', Wednesday turnover: ' + String(turnovers[839].data.Wednesday.averageTurnoverDay).slice(0,5) +
+                ', Thursday turnover: ' + String(turnovers[839].data.Thursday.averageTurnoverDay).slice(0,5) +
+                ', Friday turnover: ' + String(turnovers[839].data.Friday.averageTurnoverDay).slice(0,5) +
+                ', Saturday turnover: ' + String(turnovers[839].data.Saturday.averageTurnoverDay).slice(0,5)}
                 key={index}
                 coordinate={{
                   latitude: spot.LATITUDE,
